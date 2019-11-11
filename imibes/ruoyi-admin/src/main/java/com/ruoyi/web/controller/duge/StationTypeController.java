ba0b66887a7cbd352bb0cfb62af2ebb8b1a50e6c
ger <gergerman@126.com>
2019-11-11 18:29:53 +0800

    [project] 页面修改

---
+++ imibes/ruoyi-admin/src/main/java/com/ruoyi/web/controller/duge/StationTypeController.java
@@ -0,0 +1,94 @@
+package com.ruoyi.web.controller.duge;
+
+import com.ruoyi.common.annotation.Log;
+import com.ruoyi.common.base.AjaxResult;
+import com.ruoyi.common.enums.BusinessType;
+import com.ruoyi.common.utils.DateUtils;
+import com.ruoyi.duge.domain.DeptTest;
+import com.ruoyi.duge.domain.StationInfo;
+import com.ruoyi.duge.domain.StationType;
+import com.ruoyi.duge.domain.WeightData;
+import com.ruoyi.duge.mapper.StationTypeMapper;
+import com.ruoyi.duge.mapper.WeightDataMapper;
+import com.ruoyi.duge.service.IStationInfoService;
+import com.ruoyi.framework.web.page.TableDataInfo;
+import com.ruoyi.system.domain.SysDept;
+import com.ruoyi.web.core.base.BaseController;
+import org.apache.shiro.authz.annotation.RequiresPermissions;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import static com.ruoyi.common.utils.DateUtils.YYYY_MM_DD_HH_MM_SS;
+import static java.util.Comparator.comparing;
+
+/**
+ * 站点 信息操作处理
+ *
+ * @author ruoyi
+ * @date 2018-11-22
+ */
+@Controller
+@RequestMapping("/module/stationType")
+public class StationTypeController extends BaseController {
+    private String prefix = "stationType";
+
+    @Autowired
+    private StationTypeMapper stationTypeMapper;
+
+
+    @GetMapping()
+    public String stationType()
+    {
+        return prefix + "/stationType";
+    }
+
+    @GetMapping("/list")
+    @ResponseBody
+    public  List<StationType> list() {
+        List<StationType> stationTypeList = stationTypeMapper.selectStationTypeList();
+        return stationTypeList;
+    }
+
+    @GetMapping("/list2")
+    @ResponseBody
+    public  List<DeptTest> list2() {
+        List<DeptTest> stationTypeList = stationTypeMapper.selectDeptTest();
+        return stationTypeList;
+    }
+
+/*
+    */
+/**
+     * 选择部门树
+     *//*
+
+    @GetMapping("/selectDeptTree/{deptId}")
+    public String selectDeptTree(@PathVariable("deptId") Long deptId, ModelMap mmap)
+    {
+        mmap.put("dept", deptService.selectDeptById(deptId));
+        return prefix + "/tree";
+    }
+
+    */
+/**
+     * 加载部门列表树
+     *//*
+
+    @GetMapping("/treeData")
+    @ResponseBody
+    public List<Map<String, Object>> treeData()
+    {
+        List<Map<String, Object>> tree = deptService.selectDeptTree();
+        return tree;
+    }
+
+*/
+
+}
