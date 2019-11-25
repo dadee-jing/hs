f9fa14be096ba2aace8e6dc4a4078f12dce306f9
Nirvana <1123725815@qq.com>
2019-11-25 14:34:10 +0800

    保底删除三天前交警的图片

--- imibes/ruoyi-third/src/main/java/com/ruoyi/duge/third/trafficPolice/scheduled/TrafficPoliceClear.java
+++ imibes/ruoyi-third/src/main/java/com/ruoyi/duge/third/trafficPolice/scheduled/TrafficPoliceClear.java
@@ -1,5 +1,23 @@
 package com.ruoyi.duge.third.trafficPolice.scheduled;
 
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+/**
+ * 保底删除三天前交警的图片
+ */
+@Component
 public class TrafficPoliceClear {
-
+    Calendar calendar = Calendar.getInstance();
+    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+    @Scheduled(cron = "0 0 1 * * ?")
+    private void deletePasscar3DaysAgo(){
+        calendar.add(Calendar.DATE, -3);
+        String passcarPath="/sharedata/ftp/passcarbak/"+sdf.format(calendar.getTime());
+        new File(passcarPath).delete();
+        String peccancyPath="/sharedata/ftp/peccancybak/"+sdf.format(calendar.getTime());
+        new File(peccancyPath).delete();
+    }
 }
